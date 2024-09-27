0431b6c3eb87cde27f56aae15010c108f43c1cc8
Micha <michael.goette@posteo.de>
2024-09-27 12:15:11 +0200

    first parsing added

--- src/nomad_hysprint/parsers/__init__.py
+++ src/nomad_hysprint/parsers/__init__.py
@@ -8,9 +8,25 @@ class HySprintParserEntryPoint(ParserEntryPoint):
         return HySprintParser(**self.dict())
 
 
+class HySprintExperimentParserEntryPoint(ParserEntryPoint):
+
+    def load(self):
+        from nomad_hysprint.parsers.hysprint_batch_parser import HySprintExperimentParser
+        return HySprintExperimentParser(**self.dict())
+
+
 hysprint_parser = HySprintParserEntryPoint(
     name='HySprintParser',
     description='Parser for Hysprint files',
     mainfile_name_re='^(.+\.?.+\.((eqe|jv|jvi|pl|pli|hy|spv|env|uvvis|PL|JV|PLI|EQE|SEM|sem|xrd|mppt)\..{1,4})|.+\.nk)$',
+    mainfile_mime_re='(application|text|image)/.*'
+)
+
+
+hysprint_experiment_parser = HySprintExperimentParserEntryPoint(
+    name='HySprintParser',
+    description='Parser for Hysprint files',
+    mainfile_name_re='^(.+\.xlsx)$',
+    mainfile_content_re='Experiment Info',
     mainfile_mime_re='(application|text|image)/.*'
 )
