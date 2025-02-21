4eeee0d48f922924cc1f70bb973b06e9d6df8bdb
Michael Götte <36420750+RoteKekse@users.noreply.github.com>
2025-02-21 14:02:38 +0100

    added simulated solarcells (#19)

--- src/nomad_hysprint/schema_packages/solai_package.py
+++ src/nomad_hysprint/schema_packages/solai_package.py
@@ -48,7 +48,15 @@ class SOLAI_PLImage(ArchiveSection):
 class SOLAI_JVMeasurement(ArchiveSection):
     m_def = Section(label_quantity='name')
     name = Quantity(type=str)
-    scan_type = Quantity(type=MEnum('Forward Light', 'Reverse Light', 'Forward Dark', 'Reverse Dark'))
+    scan_type = Quantity(
+        type=MEnum(
+            'Forward Light',
+            'Reverse Light',
+            'Forward Dark',
+            'Reverse Dark',
+            'Simulated',
+        )
+    )
 
     jv_measurement = Quantity(
         type=ArchiveSection,
@@ -114,12 +122,108 @@ class SOLAI_UVVISMeasurement(ArchiveSection):
     )
 
 
+class SOLAI_EQEMeasurement(ArchiveSection):
+    m_def = Section(label_quantity='name')
+    name = Quantity(type=str)
+
+    eqe_measurement = Quantity(
+        type=ArchiveSection,
+    )
+    intensity = Quantity(
+        links=['https://purl.archive.org/tfsco/TFSCO_00001128'],
+        type=np.dtype(np.float64),
+        shape=['*'],
+    )
+
+    wavelength = Quantity(
+        links=[
+            'http://purl.obolibrary.org/obo/PATO_0001242',
+            'https://purl.archive.org/tfsco/TFSCO_00002040',
+        ],
+        type=np.dtype(np.float64),
+        shape=['*'],
+        unit='nm',
+        a_plot=[
+            {
+                'x': 'wavelength',
+                'y': 'intensity',
+                'layout': {
+                    'yaxis': {'fixedrange': False},
+                    'xaxis': {'fixedrange': False},
+                },
+                'config': {'editable': True, 'scrollZoom': True},
+            }
+        ],
+    )
+
+
+class SOLAI_SimulationInput(ArchiveSection):
+    variation_name = Quantity(
+        type=str,
+        description="""
+        The name or identifier of a paramter which is varied over a simulation
+        """,
+    )
+
+    variation_value_number = Quantity(
+        type=float,
+        description="""
+        The numerical value of a continous parameter which is varied over a simulation
+        """,
+    )
+
+    variation_value_unit = Quantity(
+        type=str,
+        description="""
+        The unit of a continous parameter which is varied over a simulation
+        """,
+    )
+
+    variation_value_string = Quantity(
+        type=str,
+        description="""
+        The string value of a categorical paramter which is varied over a simulation
+        """,
+    )
+
+
+def cell_params(voltage, current_density):
+    """
+    Calculates basic solar cell parametes form a current density (mA/cm**2)
+    voltage (V) curve.
+
+    Returns:
+        Voc (V) open circuit voltage
+        Jsc (mA/cm**2) short circuit current density
+        FF fill factor in absolute values (0-1)
+        efficiency power conversion efficiency in percentage (0-100)
+    """
+    from scipy import interpolate
+
+    j_v_interpolated = interpolate.interp1d(current_density, voltage, fill_value='extrapolate')
+    Voc = j_v_interpolated(0).item()
+    v_j_interpolated = interpolate.interp1d(voltage, current_density, fill_value='extrapolate')
+    Isc = v_j_interpolated(0).item()
+    if Isc >= 0:
+        idx = np.argmax(voltage * current_density)
+    else:
+        idx = np.argmin(voltage * current_density)
+    Vmp = voltage[idx]
+    Imp = current_density[idx]
+    Isc = abs(Isc)
+    FF = abs(Vmp.magnitude * Imp.magnitude / (Voc * Isc))
+    efficiency = Voc * FF * Isc
+    return Voc, Isc, FF, efficiency
+
+
 class SOLAI_SolarCell(CompositeSystem, PlotSection, EntryData):
     m_def = Section(
         a_eln=dict(
-            hide=['results', 'components', 'elemental_composition'],
+            hide=['results', 'elemental_composition', 'components'],
         ),
     )
+
+    simulated = Quantity(type=bool)
 
     description = Quantity(
         type=str,
@@ -135,6 +239,8 @@ class SOLAI_SolarCell(CompositeSystem, PlotSection, EntryData):
     jv_measurements = SubSection(section_def=SOLAI_JVMeasurement, repeats=True)
     jv_measurements_dark = SubSection(section_def=SOLAI_JVMeasurement, repeats=True)
     uvvis_measurements = SubSection(section_def=SOLAI_UVVISMeasurement, repeats=True)
+    eqe_measurements = SubSection(section_def=SOLAI_EQEMeasurement, repeats=True)
+    simulation_inputs = SubSection(section_def=SOLAI_SimulationInput, repeats=True)
 
     def set_solar_cell_params(self, archive, voc, jsc, ff, eff, intensity):
         solar_cell = archive.results.properties.optoelectronic.solar_cell
@@ -183,20 +289,41 @@ class SOLAI_SolarCell(CompositeSystem, PlotSection, EntryData):
                 self.jv_measurements[max_idx].jv_measurement.light_intensity,
             )
 
-    def normalize(self, archive, logger):
+    def get_solar_cell_params_3(self, archive):
+        # export to results section
+        max_idx = -1
+        eff = -1
+        params = []
+        for i, curve in enumerate(self.jv_measurements):
+            params.append(cell_params(curve.voltage, curve.current_density))
+            if params[i][3] > eff:
+                eff = params[i][3]
+                max_idx = i
+        if max_idx >= 0:
+            self.set_solar_cell_params(
+                archive,
+                params[max_idx][0],
+                params[max_idx][1],
+                params[max_idx][2],
+                params[max_idx][3],
+                None,
+            )
+
+    def normalize(self, archive, logger):  # noqa: PLR0912
         super(CompositeSystem, self).normalize(archive, logger)
         import pandas as pd
         import plotly.graph_objects as go
 
         result_figures = []
         # overview
-        overview = pd.DataFrame(columns=['Synthesis', 'JV', 'JV Dark', 'PLI', 'UVvis'])
+        overview = pd.DataFrame(columns=['Synthesis', 'JV', 'JV Dark', 'PLI', 'UVvis', 'EQE'])
         overview.loc[len(overview)] = [
             f'{len(self.synthesis_processes) if self.synthesis_processes else 0} Synthesis Processes',  # noqa E501
             f'{len(self.jv_measurements) if self.jv_measurements else 0} JV curves',
             f'{len(self.jv_measurements_dark) if self.jv_measurements_dark else 0} JV curves dark',  # noqa E501
             f'{len(self.pl_images) if self.pl_images else 0} PL Images',
             f'{len(self.uvvis_measurements) if self.uvvis_measurements else 0} UVvis curves',  # noqa E501
+            f'{len(self.eqe_measurements) if self.eqe_measurements else 0} EQE curves',  # noqa E501
         ]
 
         fig_overview = go.Figure(
@@ -235,7 +362,7 @@ class SOLAI_SolarCell(CompositeSystem, PlotSection, EntryData):
                     if 2 * i + j >= number_of_images:
                         continue
                     fig.add_trace(
-                        go.Heatmap(z=self.pl_images[2 * i + j].pl_image_data, colorscale='gray'),
+                        go.Heatmap(z=self.pl_images[i].pl_image_data, colorscale='gray'),
                         i + 1,
                         j + 1,
                     )
@@ -321,13 +448,55 @@ class SOLAI_SolarCell(CompositeSystem, PlotSection, EntryData):
                 )
             )
 
+        if self.eqe_measurements:
+            import pandas as pd
+
+            # Light JV curves
+            fig2 = go.Figure()
+            traces = []
+            for curve in self.eqe_measurements:
+                show = True
+                traces.append(
+                    go.Scatter(
+                        x=curve.wavelength.to('nm'),
+                        y=curve.intensity,
+                        name=curve.name,
+                        visible=show,
+                    )
+                )
+            fig2.add_traces(traces)
+            layout_settings = {
+                'title': 'EQE Curves',
+                'xaxis': {'title': 'Wavelength (nm)', 'gridcolor': 'lightgrey'},
+                'yaxis': {
+                    'title': 'Intensity',
+                    'showgrid': True,
+                    'gridcolor': 'lightgrey',
+                },
+                'showlegend': True,
+                'legend': {'x': 0.02, 'y': 0.98},
+                'plot_bgcolor': 'white',
+            }
+            fig2.update_layout(layout_settings)
+            result_figures.append(
+                PlotlyFigure(
+                    label='EQE Curves',
+                    index=2,
+                    open=True,
+                    figure=fig2.to_plotly_json(),
+                )
+            )
+
         self.figures = result_figures
         add_solar_cell(archive)
         if self.jv_measurements:
             try:
                 self.get_solar_cell_params_1(archive)
             except Exception:
-                self.get_solar_cell_params_2(archive)
+                try:
+                    self.get_solar_cell_params_2(archive)
+                except Exception:
+                    self.get_solar_cell_params_3(archive)
 
         archive.results.properties.optoelectronic.solar_cell.device_stack = []
         archive.results.properties.optoelectronic.solar_cell.substrate = []
