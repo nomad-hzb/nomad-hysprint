814e191a046f4ef441ba98660513ee4ab5d20f0d
Michael Götte <36420750+RoteKekse@users.noreply.github.com>
2025-04-28 13:39:25 +0200

    53 add voila button and app to plugin (#54)
    * added voila app with launch button
    
    * linting
    
    * updated version dependency
    
    * updated version dependency

---
+++ src/nomad_hysprint/apps/__init__.py
@@ -0,0 +1,94 @@
+from nomad.config.models.plugins import AppEntryPoint
+from nomad.config.models.ui import (
+    App,
+    Column,
+    FilterMenu,
+    FilterMenus,
+    FilterMenuSizeEnum,
+    Filters,
+    Format,
+    ModeEnum,
+    RowActionNorth,
+    RowActions,
+    RowDetails,
+    Rows,
+    RowSelection,
+)
+
+schema_name = 'nomad_hysprint.schema_packages.hysprint_package.HySprint_VoilaNotebook'
+hysprint_voila_app = AppEntryPoint(
+    name='voila',
+    description='Find and launch your Voila Tools.',
+    app=App(
+        # Label of the App
+        label='Voila',
+        # Path used in the URL, must be unique
+        path='voila',
+        # Used to categorize apps in the explore menu
+        category='use cases',
+        # Brief description used in the app menu
+        description='Find and launch your Voila Tools.',
+        # Longer description that can also use markdown
+        readme='Find and launch your Voila Tools.',
+        # Controls the available search filters. If you want to filter by
+        # quantities in a schema package, you need to load the schema package
+        # explicitly here. Note that you can use a glob syntax to load the
+        # entire package, or just a single schema from a package.
+        filters=Filters(
+            include=[
+                f'*#{schema_name}',
+            ]
+        ),
+        # Dictionary of search filters that are always enabled for queries made
+        # within this app. This is especially important to narrow down the
+        # results to the wanted subset. Any available search filter can be
+        # targeted here. This example makes sure that only entries that use
+        # MySchema are included.
+        filters_locked={'section_defs.definition_qualified_name': schema_name},
+        filter_menus=FilterMenus(
+            options={
+                'custom_quantities': FilterMenu(label='Notebooks', size=FilterMenuSizeEnum.L),
+                'author': FilterMenu(label='Author', size=FilterMenuSizeEnum.M),
+                'metadata': FilterMenu(label='Visibility / IDs'),
+            }
+        ),
+        columns=[
+            Column(quantity=f'data.name#{schema_name}', selected=True),
+            Column(quantity='entry_type', label='Entry type', align='left', selected=True),
+            Column(
+                quantity='entry_create_time',
+                label='Entry time',
+                align='left',
+                selected=True,
+                format=Format(mode=ModeEnum.DATE),
+            ),
+            Column(
+                quantity='upload_name',
+                label='Upload name',
+                align='left',
+                selected=True,
+            ),
+            Column(
+                quantity='authors',
+                label='Authors',
+                align='left',
+                selected=True,
+            ),
+            Column(quantity='entry_id'),
+            Column(quantity='upload_id'),
+            Column(quantity=f'data.notebook_file#{schema_name}'),
+        ],
+        rows=Rows(
+            actions=RowActions(
+                options={
+                    'launch': RowActionNorth(
+                        tool_name='voila',
+                        filepath=f'data.notebook_file#{schema_name}',
+                    )
+                }
+            ),
+            details=RowDetails(),
+            selection=RowSelection(),
+        ),
+    ),
+)
