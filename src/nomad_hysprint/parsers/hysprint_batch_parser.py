2ef87e5149fddb96f6812e8322c987928d49b1b4
Dimitra Gkogkou <a5263@nb-hp1ds64.basisit.de>
2025-06-25 15:48:57 +0200

    added datetime in parser and test

--- src/nomad_hysprint/parsers/hysprint_batch_parser.py
+++ src/nomad_hysprint/parsers/hysprint_batch_parser.py
@@ -119,6 +119,7 @@ class HySprintExperimentParser(MatchingParser):
         archives = [map_batch(sample_ids, batch_id, upload_id, HySprint_Batch)]
         substrates = []
         substrates_col = [
+            'Date',
             'Sample dimension',
             'Sample area [cm^2]',
             'Pixel area [cm^2]',
