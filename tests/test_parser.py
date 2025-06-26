a2430f04e4f15dfbe3a7f9166113e415a8843d67
paolog8 <paolograniero8@gmail.com>
2025-06-26 16:54:24 +0200

    Update batch name regex assertion in hy_batch_parser test to correct test file

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -60,7 +60,7 @@ def test_hy_batch_parser(excel_file, monkeypatch):  # noqa: PLR0915
                 assert m.data.description == 'A'
                 assert m.data.number_of_junctions == 1
             if 'Batch' in str(type(m.data)):
-                assert re.match(r'HZB_Test_\d+_\d+$', m.data.name) is not None
+                assert re.match(r'hzb_TestP_AA_\d+$', m.data.name) is not None
         elif 'Substrate' in str(type(m.data)):
             assert m.data.solar_cell_area == 10 * ureg('cm**2')
             assert m.data.pixel_area == 0.16 * ureg('cm**2')
