6e365593e823bfc6fe7d1234aaa3eaad6c8966d8
Michael Götte <36420750+RoteKekse@users.noreply.github.com>
2025-07-08 16:55:42 +0200

    Update ivoc qfsl (#104)
    * added ivoc and ivoc htet to AbsPL2
    
    * updated test
    
    * added test file
    
    * checking if dark counts exist
    
    * updated test
    
    * fixing tests
    
    * fixing tests
    
    * Linting

--- tests/test_abspl_parser.py
+++ tests/test_abspl_parser.py
@@ -31,9 +31,33 @@ def test_hysprint_abspl_parser(monkeypatch):
     # Test data exists
     assert archive.data
     assert archive.data.results
+    assert len(archive.data.results[0].wavelength) > 0
+    assert len(archive.data.results[0].luminescence_flux_density) > 0
+    assert len(archive.data.results[0].raw_spectrum_counts) > 0
+    assert len(archive.data.results[0].dark_spectrum_counts) > 0
     assert archive.data.results[0].bandgap == 1.424 * ureg('eV')
     assert archive.data.results[0].derived_jsc == 26.46 * ureg('mA/cm**2')
     assert archive.data.results[0].quasi_fermi_level_splitting == 1.094 * ureg('eV')
+
+    # Clean up
+    delete_json()
+
+
+def test_hysprint_abspl_parser_hy(monkeypatch):
+    file = '100_40-1.abspl.txt'
+    archive = get_archive(file, monkeypatch)
+    normalize_all(archive)
+
+    # Test data exists
+    assert archive.data
+    assert archive.data.results
+    assert archive.data.results[0].bandgap == 1.671 * ureg('eV')
+    assert archive.data.results[0].quasi_fermi_level_splitting == 1.168 * ureg('eV')
+    assert archive.data.results[0].i_voc == 1.172 * ureg('V')
+    assert len(archive.data.results[0].wavelength) > 0
+    assert len(archive.data.results[0].luminescence_flux_density) > 0
+    assert len(archive.data.results[0].raw_spectrum_counts) > 0
+    assert len(archive.data.results[0].dark_spectrum_counts) == 0
 
     # Clean up
     delete_json()
