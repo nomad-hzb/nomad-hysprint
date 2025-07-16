bf4f61576142f254d5ac52a6705fd430dec00c4e
Micha <michael.goette@posteo.de>
2025-07-16 19:38:24 +0200

    changed XPS to PES

--- src/nomad_hysprint/parsers/__init__.py
+++ src/nomad_hysprint/parsers/__init__.py
@@ -29,7 +29,7 @@ class SolarTabSimulationParserEntryPoint(ParserEntryPoint):
 hysprint_parser = HySprintParserEntryPoint(
     name='HySprintParser',
     description='Parser for Hysprint files',
-    mainfile_name_re='^(.+\.?.+\.((eqe|jv|jvi|pl|pli|hy|spv|env|uvvis|PL|JV|PLI|EQE|SEM|sem|xrd|mppt|xps|XPS|abspl)\..{1,4})|.+\.nk)$',
+    mainfile_name_re='^(.+\.?.+\.((eqe|jv|jvi|pl|pli|hy|spv|env|uvvis|PL|JV|PLI|EQE|SEM|sem|xrd|mppt|pes|PES|abspl)\..{1,4})|.+\.nk)$',
     mainfile_mime_re='(application|text|image)/.*',
 )
 
