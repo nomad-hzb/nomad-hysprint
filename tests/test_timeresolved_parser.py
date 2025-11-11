8e3d2a9501d8eb6ee57aa57b271eae5f81335e62
Micha <michael.goette@posteo.de>
2025-11-11 12:33:22 +0100

    clean up tests

--- tests/test_timeresolved_parser.py
+++ tests/test_timeresolved_parser.py
@@ -1,26 +1,6 @@
-import pytest
 from nomad.client import normalize_all
 
 from utils import delete_json, get_archive
-
-
-@pytest.fixture(
-    params=[
-        'HZB_MiGo_20230913_Batch-Test-1_0_0.notessfdsf.jv.txt',
-        'HZB_MMX_B7_0_C-16.pxA_ch1_LS000min_2024-11-21T12-53-39_rev.jv.txt',
-        'SE-ALM_RM_20231004_RM_KW40_0_8.jv.txt',
-    ]
-)
-def parsed_archive(request, monkeypatch):
-    """
-    Sets up data for testing and cleans up after the test.
-    """
-    yield get_archive(request.param, monkeypatch)
-
-
-def test_normalize_all(parsed_archive, monkeypatch):
-    normalize_all(parsed_archive)
-    delete_json()
 
 
 def test_trpl_parser_1(monkeypatch):
